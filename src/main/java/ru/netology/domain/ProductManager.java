1eb74d8d5e74df80a8c9f3d37adaa63a6c425251
Dmitrii Nasedkin <2_tone@mail.ru>
2021-05-26 00:59:15 +0300

    added classes Book, Smartphone, Repository and Manager - the first version

---
+++ src/main/java/ru/netology/domain/ProductManager.java
@@ -0,0 +1,8 @@
+package ru.netology.domain;
+
+
+public class ProductManager {
+    private Product[] products = new Product[0];
+
+
+}
---
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -0,0 +1,45 @@
+package ru.netology.domain;
+
+import java.util.Objects;
+
+public class Smartphone extends Product {
+  private String manufacturer;
+
+  public Smartphone () {super();}
+
+  public Smartphone(int id, String name, int price, String manufacturer) {
+    super(id, name, price);
+    this.manufacturer = manufacturer;
+  }
+
+  public String getManufacturer() {
+    return manufacturer;
+  }
+
+  public void setManufacturer(String manufacturer) {
+    this.manufacturer = manufacturer;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    if (!super.equals(o)) return false;
+    Smartphone that = (Smartphone) o;
+    return Objects.equals(manufacturer, that.manufacturer);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), manufacturer);
+  }
+
+  @Override
+  public String toString() {
+    return "Smartphone{" +
+            "manufacturer='" + manufacturer + '\'' +
+            '}';
+  }
+}
+
+
